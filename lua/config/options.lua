b0a75023ca694a2ba15294dce87b03059c71f5c3
Jack Neilson <j.neilson@jfdglobal.com>
2025-08-15 09:16:06 +0100

    First commit

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.ai_cmp = false
+LazyVim.terminal.setup("powershell")
+vim.opt.softtabstop = 2
+vim.opt.incsearch = true
---
+++ lua/plugins/blink.lua
@@ -0,0 +1,13 @@
+return {
+  "Saghen/blink.cmp",
+  opts = function(_, opts) 
+    opts.sources.default = { 'lsp', 'buffer', 'path', 'lazydev' }
+    opts.sources.providers = {
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        score_offset = 100
+      }
+    }
+  end
+}
